102090c8916176cb3b2d729fd051d2a2e5a116dd
Krissh Diwedy <krisshdiwedy38@gmail.com>
2024-07-03 21:49:41 +0530

    Add files via upload

---
+++ lp-script.js
@@ -0,0 +1,18 @@
+/*
+
+const toggle = document.querySelectorAll('.CAD');
+const form = document.getElementById('form-container');
+const closing = document.getElementById('close-button')
+
+toggle.forEach((CAD) => {
+   CAD.addEventListener('click' , () => {
+      form.style.visibility = 'visible';
+})
+})
+
+closing.addEventListener('click', () => {
+    form.style.visibility = "hidden";
+})
+
+*/
+
