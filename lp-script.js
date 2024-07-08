5c7f132be872cf04e3ceb238f07665b7d8228029
Krissh Diwedy <krisshdiwedy38@gmail.com>
2024-07-09 03:03:04 +0530

    Update lp-script.js

--- lp-script.js
+++ lp-script.js
@@ -16,3 +16,38 @@ closing.addEventListener('click', () => {
 
 */
 
+
+const calendarPic = document.getElementById('calendar');
+const todoPic = document.getElementById('todo');
+const journalPic = document.getElementById('journal');
+const trackerPic = document.getElementById('track');
+const calendarTag = document.getElementById('calendar-tag');
+const todoTag = document.getElementById('todo-tag');
+const journalTag = document.getElementById('journal-tag');
+const trackerTag = document.getElementById('trackers-tag');
+
+calendarTag.addEventListener('click', () => {
+    calendarPic.style.visibility ='visible';
+    todoPic.style.visibility = 'hidden';
+    journalPic.style.visibility='hidden';
+    trackerPic.style.visibility='hidden';
+})
+
+todoTag.addEventListener('click', () => {
+    calendarPic.style.visibility ='hidden';
+    todoPic.style.visibility = 'visible';
+    journalPic.style.visibility='hidden';
+    trackerPic.style.visibility='hidden';
+})
+journalTag.addEventListener('click', () => {
+    calendarPic.style.visibility ='hidden';
+    todoPic.style.visibility = 'hidden';
+    journalPic.style.visibility='visible';
+    trackerPic.style.visibility='hidden';
+})
+trackerTag.addEventListener('click', () => {
+    calendarPic.style.visibility ='hidden';
+    todoPic.style.visibility = 'hidden';
+    journalPic.style.visibility='hidden';
+    trackerPic.style.visibility='visible';
+})
